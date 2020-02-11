af3a2b01cbfd3a044a5b0235df9c4fd75b88e251
David Roundy <roundyd@physics.oregonstate.edu>
2020-02-11 09:50:26 -0800

    add an actual test

--- admin_app/tests.py
+++ admin_app/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from admin_app.models import Activity
 
 # Create your tests here.
+
+class ActivityTestCase(TestCase):
+    def setUp(self):
+        Activity.objects.create(
+            title = "Test activity",
+            overview_paragraph = "This is only a test",
+        )
+
+    def test_activity_exists(self):
+        """Just checks that my test activity exists"""
+        test = Activity.objects.get(title="Test activity")
+        self.assertEqual(test.overview, 'This is only a test')
