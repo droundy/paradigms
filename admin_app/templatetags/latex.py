5a8d598477bec9487d51858a5d8f013e360a48cb
David Roundy <roundyd@physics.oregonstate.edu>
2020-02-28 06:32:34 -0800

    add zero draft of a custom filter for latex

---
+++ admin_app/templatetags/latex.py
@@ -0,0 +1,20 @@
+from django import template
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
+import latex_snippets
+
+register = template.Library()
+
+@register.filter()
+def latex_with_solution(value):
+    if value:
+        return latex_snippets.html_with_solution(value)
+    else:
+        return value
+
+@register.filter()
+def latex_omit_solution(value):
+    if value:
+        return latex_snippets.html_omit_solution(value)
+    else:
+        return value
