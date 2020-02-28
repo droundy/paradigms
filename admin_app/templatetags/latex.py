bebf5ca722ff19aac92e1b25b25b1eea9a52560a
David Roundy <roundyd@physics.oregonstate.edu>
2020-02-28 12:31:44 -0800

    avoid problematic import of latex_snippet (broken?)

--- admin_app/templatetags/latex.py
+++ admin_app/templatetags/latex.py
@@ -1,20 +1,22 @@
 from django import template
 from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
-import admin_app.templatetags.latex_snippet
+# import admin_app.templatetags.latex_snippet
 
 register = template.Library()
 
 @register.filter()
 def latex_with_solution(value):
     if value:
-        return latex_snippets.html_with_solution(value)
+        # return latex_snippets.html_with_solution(value)
+        return 'latex: '+value
     else:
         return value
 
 @register.filter()
 def latex_omit_solution(value):
     if value:
-        return latex_snippets.html_omit_solution(value)
+        # return latex_snippets.html_omit_solution(value)
+        return 'latex sans solution: '+value
     else:
         return value
