8e700510eee9648ef26690a72732bcf620a97c6b
David Roundy <roundyd@physics.oregonstate.edu>
2020-02-28 12:23:00 -0800

    attempt to fix the path of latex_snippets import

--- admin_app/templatetags/latex.py
+++ admin_app/templatetags/latex.py
@@ -1,7 +1,7 @@
 from django import template
 from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
-import latex_snippets
+import admin_app.templatetags.latex_snippets
 
 register = template.Library()
 
