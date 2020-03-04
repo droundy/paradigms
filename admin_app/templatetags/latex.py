971c96f79686133f34c193813259417187e26407
David Roundy <roundyd@physics.oregonstate.edu>
2020-03-04 14:41:22 -0800

    use new custom filter to render latex on "pages"

--- admin_app/templatetags/latex.py
+++ admin_app/templatetags/latex.py
@@ -1,22 +1,20 @@
 from django import template
 from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
-# import latex_snippet
+import latex_snippet
 
 register = template.Library()
 
 @register.filter()
 def latex_with_solution(value):
     if value:
-        # return latex_snippets.html_with_solution(value)
-        return 'latex: '+value
+        return mark_safe(latex_snippet.html_with_solution(value))
     else:
         return value
 
 @register.filter()
 def latex_omit_solution(value):
     if value:
-        # return latex_snippets.html_omit_solution(value)
-        return 'latex sans solution: '+value
+        return mark_safe(latex_snippet.html_omit_solution(value))
     else:
         return value
