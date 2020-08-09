a347c8cda14c7b271c8a54c61ff8d84bfab15a2a
David Roundy <daveroundy@gmail.com>
2020-08-09 13:12:53 -0700

    allow a test domain

--- osu_www/settings.py
+++ osu_www/settings.py
@@ -50,7 +50,7 @@ SECRET_KEY = '*%bxc)s2u(_e9%6)@-i#jqs4-%48qj93wq&)lh=4fgx8o-oggi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','10.0.20.100','paradigms.oregonstate.edu','paradigms.physics.oregonstate.edu','osu.wiki','development.osu.wiki','10.0.42.189','10.0.42.188','osu.wiki:8263']
+ALLOWED_HOSTS = ['127.0.0.1','10.0.20.100','paradigms.oregonstate.edu','paradigms.physics.oregonstate.edu','osu.wiki','development.osu.wiki','10.0.42.189','10.0.42.188','osu.wiki:8263', 'roundy.is-a-geek.com']
 
 
 # Application definition
