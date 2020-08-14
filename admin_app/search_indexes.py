15341d6b4c70f1c1a82aa2a3b164a09051f7e0b2
David Roundy <daveroundy@gmail.com>
2020-08-14 11:10:56 -0700

    index problems for search

--- admin_app/search_indexes.py
+++ admin_app/search_indexes.py
@@ -1,6 +1,6 @@
 import datetime
 from haystack import indexes
-from .models import Activity
+from .models import Activity, Problem
 
 
 class ActivityIndex(indexes.SearchIndex, indexes.Indexable):
@@ -11,6 +11,19 @@ class ActivityIndex(indexes.SearchIndex, indexes.Indexable):
 
     def get_model(self):
         return Activity
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.all()
+
+class ProblemIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+
+    #topic = indexes.CharField(model_attr='topics')
+    #equipment = indexes.CharField(model_attr='equipment_required')
+
+    def get_model(self):
+        return Problem
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
