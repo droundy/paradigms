a38d4e0c3c52e6cd9785160a444f59eb65aba65e
David Roundy <daveroundy@gmail.com>
2020-08-14 09:40:25 -0700

    add a bit of editing to edit page

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,7 @@
 #from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 from django.db import IntegrityError, transaction
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.forms.formsets import formset_factory
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -12,6 +12,7 @@ from django.contrib.auth.models import Group, User
 from django.contrib.auth import get_user_model
 from django.contrib import messages
 from django.views.decorators.csrf import csrf_exempt
+import django
 from subprocess import Popen, PIPE, STDOUT
 import subprocess
 import os
@@ -50,10 +51,25 @@ def course_as_taught(request, number, year, view='overview'):
         'view': view,
     })
 
+@permission_required('admin_app.edit_course_as_taught',login_url='/')
 def course_as_taught_edit(request, number, year):
     # if this is a POST request we need to process the form data
     course = get_object_or_404(Course, number=number)
-    as_taught = get_object_or_404(CourseAsTaught, course=course, slug=year)
+    if year == 'NEW' and CourseAsTaught.objects.filter(course=course, slug='NEW').count() == 0:
+        as_taught = CourseAsTaught(course=course, year='NEW', instructor=request.user.get_full_name())
+        as_taught.save()
+        print('creating new course')
+    else:
+        as_taught = get_object_or_404(CourseAsTaught, course=course, slug=year)
+    
+    if request.method == 'POST':
+        as_taught.instructor = request.POST['instructor']
+        as_taught.year = request.POST['year']
+        as_taught.slug = as_taught.year.replace(' ', '')
+        as_taught.save()
+        if as_taught.slug != year:
+            return HttpResponseRedirect(django.urls.reverse('course_as_taught_edit', args=(number, as_taught.slug)))
+
     return render(request, 'courses/taught-edit.html', {
         'course': course,
         'taught': as_taught,
