5c9d643a13f13d4c27f108fc04c5baf741c3e431
David Roundy <daveroundy@gmail.com>
2020-09-09 15:18:51 -0700

    enable show problemset

--- osu_www/courses/views.py
+++ osu_www/courses/views.py
@@ -95,6 +95,7 @@ def course_as_taught_edit(request, number, year):
                 day.order = request.POST['day-{}-order'.format(day.pk)]
                 day.problemsetname = request.POST['day-{}-problemset'.format(day.pk)]
                 day.show_solution = 'day-{}-solution'.format(day.pk) in request.POST
+                day.show_problemset = 'day-{}-show-problemset'.format(day.pk) in request.POST
                 for da in day.dayactivity_set.all():
                     if "day-{}-activity-{}-delete".format(day.pk, da.pk) in request.POST:
                         if not using_old_data:
