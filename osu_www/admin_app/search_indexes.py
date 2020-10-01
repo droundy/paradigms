756b13e937fe521f481b45ebf78a8cd6de1c8252
David Roundy <daveroundy@gmail.com>
2020-10-01 16:39:02 -0700

    first stab at including sequences in the new search

--- osu_www/admin_app/search_indexes.py
+++ osu_www/admin_app/search_indexes.py
@@ -1,6 +1,6 @@
 import datetime
 from haystack import indexes
-from .models import Activity, Problem
+from .models import Activity, Problem, Sequence
 
 
 class ActivityIndex(indexes.SearchIndex, indexes.Indexable):
@@ -24,6 +24,19 @@ class ProblemIndex(indexes.SearchIndex, indexes.Indexable):
 
     def get_model(self):
         return Problem
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.all()
+
+class SequenceIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+
+    #topic = indexes.CharField(model_attr='topics')
+    #equipment = indexes.CharField(model_attr='equipment_required')
+
+    def get_model(self):
+        return Sequence
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
